9cc2f912ec47cef67f0d59894718f32fc3ba7f5c
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-24 11:39:28 -0300

    Adicionando validações

--- src/main/java/com/github/cidarosa/ms/produto/controller/ProdutoController.java
+++ src/main/java/com/github/cidarosa/ms/produto/controller/ProdutoController.java
@@ -2,6 +2,7 @@ package com.github.cidarosa.ms.produto.controller;
 
 import com.github.cidarosa.ms.produto.dto.ProdutoDTO;
 import com.github.cidarosa.ms.produto.service.ProdutoService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -34,7 +35,7 @@ public class ProdutoController {
     }
 
     @PostMapping
-    public ResponseEntity<ProdutoDTO> createProduto(@RequestBody ProdutoDTO produtoDTO) {
+    public ResponseEntity<ProdutoDTO> createProduto(@RequestBody @Valid ProdutoDTO produtoDTO) {
         produtoDTO = produtoService.saveProduto(produtoDTO);
 
         URI uri = ServletUriComponentsBuilder
@@ -48,7 +49,7 @@ public class ProdutoController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<ProdutoDTO> updateProduto(@PathVariable Long id, @RequestBody ProdutoDTO produtoDTO) {
+    public ResponseEntity<ProdutoDTO> updateProduto(@PathVariable Long id, @RequestBody @Valid ProdutoDTO produtoDTO) {
         produtoDTO = produtoService.updateProduto(id, produtoDTO);
 
         return ResponseEntity.ok(produtoDTO);
--- src/main/java/com/github/cidarosa/ms/produto/dto/ProdutoDTO.java
+++ src/main/java/com/github/cidarosa/ms/produto/dto/ProdutoDTO.java
@@ -1,6 +1,10 @@
 package com.github.cidarosa.ms.produto.dto;
 
 import com.github.cidarosa.ms.produto.entities.Produto;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,9 +15,19 @@ import lombok.NoArgsConstructor;
 
 public class ProdutoDTO {
 
+
     private Long id;
+
+    @NotBlank(message = "Campo nome é requerido")
+    @Size(min = 10, max = 100, message = "O nome deve ter entre 10 a 100 caracteres")
     private String nome;
+
+    @NotBlank(message = "Campo descrição é requerido")
+    @Size(min = 10, message = "Descrição deve ter no mínimo 100 caracteres")
     private String descricao;
+
+    @NotNull(message = "O campo valor é requerido")
+    @Positive(message = "O campo valor deve possuir um número positivo maior que 0")
     private Double valor;
 
     public ProdutoDTO(Produto produto) {
